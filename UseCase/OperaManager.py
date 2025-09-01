28123b05d064859096a9d3cd838228e7b137ec50
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-01 16:26:39 -0300

    meu usecase para gerenciar o Opera

--- UseCase/OperaManager.py
+++ UseCase/OperaManager.py
@@ -1,22 +1,26 @@
 import pygetwindow as gw
+from Domain.Monitor import Monitor
 
 class OperaManager:
-    def __init__(self, monitor):
+
+    def __init__(self, monitor: Monitor):
         self.monitor = monitor
 
     def get_opera_windows(self):
-        return [janela for janela in gw.getAllWindows() if janela.title and "Opera" in janela.title]
+        return [w for w in gw.getAllWindows() if w.title and "Opera" in w.title]
 
-    def reposition_window(self, janela):
+    def reposition_window(self, window) -> bool:
         try:
-            if janela.isMinimized:
-                janela.restore()
-            if (janela.left == self.monitor.x and janela.top == self.monitor.y and
-                janela.width == self.monitor.width and janela.height == self.monitor.height):
+            if window.isMinimized:
+                window.restore()
+
+            if (window.left == self.monitor.x and window.top == self.monitor.y and
+                window.width == self.monitor.width and window.height == self.monitor.height):
                 return True
-            janela.moveTo(self.monitor.x, self.monitor.y)
-            janela.resizeTo(self.monitor.width, self.monitor.height)
+
+            window.moveTo(self.monitor.x, self.monitor.y)
+            window.resizeTo(self.monitor.width, self.monitor.height)
             return True
         except Exception as e:
-            print(f"[OperaManager] Erro: {e}")
+            print(f"[UseCase] Erro ao reposicionar: {e}")
             return False
